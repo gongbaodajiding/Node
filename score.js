b2f23598310fb6b7510cde84a7f91c0789744694
gbjd <wx807663414@qq.com>
2021-11-16 23:07:15 +0800

    Initial commit

---
+++ score.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+// fs.readFile('./成绩.txt', 'utf8', function (err, data) {
+//     if (err) {
+//         return console.log('读取失败' + err.message);
+//     }
+//     console.log('读取成功' + data);
+//     console.log(typeof (data));
+//     let scoreOld = data.split(' ');
+//     console.log(scoreOld);
+// });
+
+fs.readFile('./成绩.txt', 'utf8', (err, data) => {
+    // 如果出现异常直接抛出异常
+    if (err) throw err;
+    console.log('读取成功');
+
+    let scoreOld = data.split(' ');
+    let arrNew = [];
+    scoreOld.forEach(item => arrNew.push(item.replace('=', '：')));
+    let scoreNew = arrNew.join('\r\n');
+
+    fs.writeFile('./成绩新.txt', scoreNew, err => {
+        if (err) throw err;
+        console.log('写入成功');
+    });
+});
+
